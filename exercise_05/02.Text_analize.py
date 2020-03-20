3509295659278d99ce55ca7efab80ee2c2a20a5a
Kaloyan Velkov <kalo7o93@gmail.com>
2020-03-20 23:53:55 +0200

    not complete

---
+++ exercise_05/02.Text_analize.py
@@ -0,0 +1,31 @@
+string = input("Enter the stringing : ")
+
+vowels = 0
+digits = 0
+consonants = 0
+spaces = 0
+symbols = 0
+
+string = string.lower()
+
+vowels_list = ['a', 'e', 'i', 'o', 'u']
+number_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+
+for i in range(0, len(string)):
+    if(any(ele in string[i] for ele in vowels_list)):
+        vowels = vowels + 1
+    elif(any(ele in string[i] for ele in number_list)):
+        digits = digits + 1
+    elif(string[i] == ' '):
+        spaces = spaces + 1
+    elif(string[i].isalpha()):
+        consonants = consonants + 1
+    else:
+        symbols = symbols + 1
+
+print("Vowels: ", vowels)
+print("Consonants: ", consonants)
+print("Digits: ", digits)
+print("White spaces: ", spaces)
+print("Symbols : ", symbols)
---
+++ exercise_06/02.Turtle.py
@@ -0,0 +1,29 @@
+import turtle               # allows us to use the turtles library
+
+wn = turtle.Screen()        # creates a graphics window
+
+
+alex = turtle.Turtle()
+alex.forward(150)
+alex.left(135)
+alex.forward(214)
+alex.left(135)
+alex.forward(150)
+alex.left(135)
+
+alex = turtle.Turtle()
+alex.forward(150)
+alex.right(90)
+alex.forward(150)
+alex.right(90)
+alex.forward(150)
+alex.right(90)
+alex.forward(150)
+alex.right(90)
+
+alex.circle(150)
+
+wn.mainloop()
+
+
+
