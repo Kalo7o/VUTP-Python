63bd58a68e7a837308d29cbbce9e8f7fc152b106
Kaloyan Velkov <kalo7o93@gmail.com>
2020-03-22 05:04:35 +0200

    ok

--- exercise_06/01.Contact_list_OOP.py
+++ exercise_06/01.Contact_list_OOP.py
@@ -1,41 +1,12 @@
-class Contact:
-  def __init__(self, name, address, birth_day, phone_number, email, profession, interests):
-    self.name = name
-    self.address = address
-    self.birth_day = birth_day
-    self.phone_number = phone_number
-    self.email = email
-    self.profession = profession
-    self.interests = interests
-
-  def update(self):
-    print("Change Name: 1, Address: 2, Birth day: 3, Phone: 4, Email: 5, Profession: 6, Interests: 7")
-    option = int(input("Choose an parameter: "))
-    if option == 1:
-      self.name = input()
-    elif option == 2:
-      self.birth_day = input()
-    elif option == 3:
-      self.phone_number = input()
-    elif option == 4:
-      self.email = input()
-    elif option == 5:
-      self.profession = input()
-    elif option == 6:
-      self.interests = input()
-    else:
-      print("Invalid parameter")
-      return
-
-  def show(self):
-    print("name:", self.name, ", address:", self.address, ", Phone:", self.phone_number, ", email:", self.email, ", profession:", self.profession, ", interests:", self.interests)
-
+import numpy as np
+import os
 
 class AddressBook:
     def __init__(self):
         self.contact_list = []
+
     def add_contact(self):
-        contact_item = Contact(
+        contact_item = [
             input("Contact name: "),
             input("Contact address: "),
             input("Contact byrth day: "),
@@ -43,38 +14,53 @@ class AddressBook:
             input("Contact email: "),
             input("Contact profession: "),
             input("Contact interests: ")
-        )
+        ]
         self.contact_list.append(contact_item)
+
     def update_contact(self, id):
-      self.contact_list[id].update()
+        print("Change Name: 0, Address: 1, Birth day: 2, Phone: 3, Email: 4, Profession: 5, Interests: 6")
+        option = int(input("Choose an parameter: "))
+        if option >= 0 and option <= 6:
+            self.contact_list[id][option] = input()
+        else:
+            print("Invalid parameter")
+            return
 
     def delete_contact(self, id):
-      del self.contact_list[id]
+        del self.contact_list[id]
 
     def show_contacts(self):
-      for i in range(len(self.contact_list)):
-        print("ID: ", i, end = ', ')
-        self.contact_list[i].show()
-        
+        for i in range(len(self.contact_list)):
+            print("ID: ", i, ", name:", self.contact_list[i][0], ", address:", self.contact_list[i][1], ", byrth day:", self.contact_list[i][2], ", phone:",
+                  self.contact_list[i][3], ", email:", self.contact_list[i][4], ", profession:", self.contact_list[i][5], ", interests:", self.contact_list[i][6])
+
     def help(self):
-      print('Commands: add, remove, update, list, exit')
+        print('Commands: add, remove, update, list, exit')
+
+    def save(self):
+        np.save('./exercise_06/Database.npy', self.contact_list)
+        np.savetxt('./exercise_06/2darray.csv', self.contact_list, delimiter=',', fmt='%s')
+
+    def load(self):
+        self.contact_list = np.load('./exercise_06/Database.npy')
+
 
 AB = AddressBook()
 command = ''
 
 while command != 'exit':
-  command = input()
-  if command == 'add':
-    AB.add_contact()
-  elif command == 'remove':
-    AB.delete_contact(int(input('ID: ')))
-  elif command == 'update':
-    AB.update_contact(int(input('ID: ')))
-  elif command == 'list':
-    AB.show_contacts()
-  elif command == 'help':
-    AB.help()
-  else:
-    print("Invalid command, type \"help\" to view the commands.")
-
-
+    AB.load()
+    command = input()
+    if command == 'add':
+        AB.add_contact()
+    elif command == 'remove':
+        AB.delete_contact(int(input('ID: ')))
+    elif command == 'update':
+        AB.update_contact(int(input('ID: ')))
+    elif command == 'list':
+        AB.show_contacts()
+    elif command == 'help':
+        AB.help()
+    elif command != 'exit':
+        print("Invalid command, type \"help\" to view the commands.")
+    AB.save()
